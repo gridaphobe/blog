9cece2479b762439e1b66c102f994e9a8e276f98
Eric Seidel <gridaphobe@gmail.com>
2012-06-21 15:38:47 -0400

    try fixing the feed again, use URIs for post ids instead of URLs

--- src/Feed.hs
+++ src/Feed.hs
@@ -9,7 +9,7 @@ import System.Locale
 import Text.Blaze
 import Text.Blaze.Html5.Attributes (rel, href, type_)
 import Text.Blaze.Internal
-import Text.Blaze.Renderer.Utf8
+import Text.Blaze.Renderer.String
 
 import Post
 
@@ -18,11 +18,10 @@ import Debug.Trace
 feed :: [Post] -> Html
 feed ps = feed_ ! customAttribute "xmlns" "http://www.w3.org/2005/Atom" $ do
     title_ $ "Eric Seidel"
-    link_ ! rel "self" ! href "http://www.eseidel.org/atom.xml"
     link_ ! rel "self" ! href "http://eseidel.org/atom.xml"
-    link_ ! href "http://www.eseidel.org/"
+    link_ ! href "http://eseidel.org/"
     updated_ $ toHtml $ formatTime defaultTimeLocale "%FT%XZ" $ date $ head ps
-    id_ $ "http://www.eseidel.org/"
+    id_ $ "tag:gridaphobe.blog"
     author_ $ do
         name_ $ "Eric Seidel"
         email_ $ "gridaphobe@gmail.com"
@@ -32,10 +31,10 @@ feed ps = feed_ ! customAttribute "xmlns" "http://www.w3.org/2005/Atom" $ do
 entry :: Post -> Html
 entry p = entry_ $ do
     title_ $ toHtml $ title p
-    link_ ! href ("http://www.eseidel.org/posts/" `mappend` (toValue $ slug p))
+    link_ ! href ("http://eseidel.org/posts/" `mappend` (toValue $ slug p))
     updated_ $ toHtml $ formatTime defaultTimeLocale "%FT%XZ" $ date p
-    id_  $ toHtml $ "http://www.eseidel.org/posts/" `mappend` slug p
-    content_ ! customAttribute "type" "html" $ toHtml $ L.unpack $ renderHtml $ content p
+    id_  $ toHtml $ "tag:gridaphobe.blog," `mappend` slug p
+    content_ ! customAttribute "type" "html" $ toHtml $ renderHtml $ content p
 
 feed_ = Parent "feed" "<feed" "</feed>"
 entry_ = Parent "entry" "<entry" "</entry>"
