90eb65609e17b6fb450a50130a958f50c86c508c
Eric Seidel <gridaphobe@gmail.com>
2012-06-21 15:50:15 -0400

    fix tags

--- src/Feed.hs
+++ src/Feed.hs
@@ -21,7 +21,7 @@ feed ps = feed_ ! customAttribute "xmlns" "http://www.w3.org/2005/Atom" $ do
     link_ ! rel "self" ! href "http://eseidel.org/atom.xml"
     link_ ! href "http://eseidel.org/"
     updated_ $ toHtml $ formatTime defaultTimeLocale "%FT%XZ" $ date $ head ps
-    id_ $ "tag:gridaphobe.blog"
+    id_ $ "tag:gridaphobe.blog,2012:"
     author_ $ do
         name_ $ "Eric Seidel"
         email_ $ "gridaphobe@gmail.com"
@@ -33,7 +33,7 @@ entry p = entry_ $ do
     title_ $ toHtml $ title p
     link_ ! href ("http://eseidel.org/posts/" `mappend` (toValue $ slug p))
     updated_ $ toHtml $ formatTime defaultTimeLocale "%FT%XZ" $ date p
-    id_  $ toHtml $ "tag:gridaphobe.blog," `mappend` slug p
+    id_  $ toHtml $ "tag:gridaphobe.blog,2012:" `mappend` slug p
     content_ ! customAttribute "type" "html" $ toHtml $ renderHtml $ content p
 
 feed_ = Parent "feed" "<feed" "</feed>"
