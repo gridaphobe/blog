8b021454f23627b3829280bb2460f02ae2f8e1f6
Eric Seidel <gridaphobe@gmail.com>
2012-12-05 21:50:03 -0800

    remove unused files

--- cookbooks/heroku-toolbelt/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-log "Install Heroku Toolbelt..."
-execute "install heroku toolbelt" do
-  command "wget -qO- https://toolbelt.heroku.com/install.sh | sh"
-  action :run
-  not_if "which heroku"
-end
