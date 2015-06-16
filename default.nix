d15dc43891451c724d792aa125fed316ade87acd
Eric Seidel <gridaphobe@gmail.com>
2015-06-15 22:25:00 -0700

    update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-{ mkDerivation, base, blaze-html, blaze-markup, containers, css
-, directory, filepath, highlighting-kate, mtl, old-locale, pandoc
-, pandoc-types, scotty, stdenv, text, time, transformers, unix
-, wai-extra, wai-middleware-static
+{ mkDerivation, base, blaze-html, blaze-markup, clay, containers
+, directory, filepath, highlighting-kate, mtl, pandoc, pandoc-types
+, scotty, stdenv, text, time, transformers, unix, wai-extra
+, wai-middleware-static
 }:
 mkDerivation {
   pname = "blog";
@@ -10,9 +10,9 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   buildDepends = [
-    base blaze-html blaze-markup containers css directory filepath
-    highlighting-kate mtl old-locale pandoc pandoc-types scotty text
-    time transformers unix wai-extra wai-middleware-static
+    base blaze-html blaze-markup clay containers directory filepath
+    highlighting-kate mtl pandoc pandoc-types scotty text time
+    transformers unix wai-extra wai-middleware-static
   ];
   description = "Project Synopsis Here";
   license = stdenv.lib.licenses.bsd3;
