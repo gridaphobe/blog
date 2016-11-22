5431e9b5e4a9061f59b361942659fcad346824b5
Eric Seidel <eric@seidel.io>
2016-11-21 19:52:41 -0800

    hack

--- src/Main.hs
+++ src/Main.hs
@@ -37,32 +37,46 @@ main = do
 
     create ["css/style.css"] $ do
       route idRoute
-      compile $ makeItem $ T.unpack $ V.style
+      compile $ do
+        makeItem (T.unpack V.code)
+        >>= relativizeUrls
 
     create ["css/code.css"] $ do
       route idRoute
-      compile $ makeItem $ T.unpack $ V.code
+      compile $ do
+        makeItem (T.unpack V.code)
+        >>= relativizeUrls
 
     create ["index.html"] $ do
       route idRoute
-      compile $ makeItem $ T.unpack $ V.index (mostRecent 5 posts)
+      compile $ do
+        makeItem (T.unpack (V.index (mostRecent 5 posts)))
+        >>= relativizeUrls
 
     create ["feed.xml"] $ do
       route idRoute
-      compile $ makeItem $ T.unpack $ V.feed $ sorted posts
+      compile $ do
+        makeItem (T.unpack (V.feed (sorted posts)))
+        >>= relativizeUrls
 
     create ["posts/index.html"] $ do
       route idRoute
-      compile $ makeItem $ T.unpack $ V.archive $ sorted posts
+      compile $ do
+        makeItem (T.unpack (V.archive (sorted posts)))
+        >>= relativizeUrls
 
     create ["publications/index.html"] $ do
       route idRoute
-      compile $ makeItem $ T.unpack $ V.publications
+      compile $ do
+        makeItem (T.unpack V.publications)
+          >>= relativizeUrls
 
     forM_ posts $ \post -> do
       create [fromFilePath $ "posts" </> ST.unpack (slug post) </> "index.html"] $ do
         route idRoute
-        compile $ makeItem $ T.unpack $ V.post post
+        compile $ do
+          makeItem (T.unpack (V.post post))
+          >>= relativizeUrls
 
     create ["cv.pdf", "resume.pdf"] $ do
       route idRoute
